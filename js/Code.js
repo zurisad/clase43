6191fa59eef8093dafb76a7c72827724f157fede
sara <piedrasyfundamentoac@gmail.com>
2021-12-11 11:07:39 -0600

    first commit

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+     fill("white")
+     textSize(15)
+     text("C U N I F O N ", 700,150)
+     fill("lightblue")
+     text("Hint: Performs a particular task !!", 700,175)
+
+     fill("white")
+     textSize(15)
+     text("E D A T E D O S B A S ", 100,280)
+     fill("lightblue")
+     text("Hint: Stores all information !!", 100,300)
+}
